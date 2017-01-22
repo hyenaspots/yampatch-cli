a76105a8e253696ec11e59ce31367dd2dbae9123
Jesse Alford <Jesse.T.Alford@gmail.com>
2017-01-22 15:01:26 -0800

    Add file reading
    Signed-off-by: Steven Dodson <Steven.G.Dodson@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
-	fmt.Println(`YAML`)
+	if len(os.Args) < 2 {
+		fmt.Println(`YAML`)
+		os.Exit(0)
+	}
+
+	inputYAMLFile, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(inputYAMLFile))
 }
--- yampatch_cli_test.go
+++ yampatch_cli_test.go
@@ -10,12 +10,42 @@ import (
 )
 
 var _ = Describe("the yampatch cli", func() {
-	It("Builds, runs, and exits 0", func() {
-		command := exec.Command(binPath)
-		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
+	var (
+		session *gexec.Session
+		err     error
+	)
 
-		Eventually(session, 5).Should(gexec.Exit(0))
-		Eventually(session.Out, 5).Should(gbytes.Say(`YAML`))
+	Context("When you run the command without arguments", func() {
+		BeforeEach(func() {
+			command := exec.Command(binPath)
+			session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Runs, and exits 0", func() {
+			Eventually(session, 5).Should(gexec.Exit(0))
+			Eventually(session.Out, 5).Should(gbytes.Say(`YAML`))
+		})
+
+		// It("Prints an error/usage message", func() {
+		// 	Eventually(session, 5).Should(gexec.Exit(0))
+		// 	Eventually(session.Out, 5).Should(gbytes.Say(`some error`))
+		// })
+	})
+
+	Context("When you run the command with just the input file argument", func() {
+		BeforeEach(func() {
+			command := exec.Command(binPath, "fixtures/simple_target.yml")
+			session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("prints the contents of that file, unchanged, to stdout", func() {
+			Eventually(session, 5).Should(gexec.Exit(0))
+			Eventually(session.Out, 5).Should(gbytes.Say(`---
+my-key1: my-key1-starting-value
+`))
+		})
+
 	})
 })
